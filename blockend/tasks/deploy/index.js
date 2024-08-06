8732020efc74e82777828e406a4250208cd1c470
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-08-06 13:52:39 +0700

    add

--- blockend/tasks/deploy/index.js
+++ blockend/tasks/deploy/index.js
@@ -1,2 +1,4 @@
-exports.deplo
+exports.deployCore=require("./deployCore")
+exports.deployCrosschainMock=require("./deployCrosschainMock")
+exports.deployVaultImplementation=require("./deployVaultImplementation")
 exports.mocks=require("./mocks")
---
+++ blockend/tasks/deploy/mocks/index.js
@@ -0,0 +1,3 @@
+exports.deployCoreMock = require("./deployCoreMock");
+exports.deployCrosschainMock = require("./deployCrosschainMock");
+exports.deployVaultImplementation = require("./deployVaultImplementation");
