60d99479aab31bc238b0b0020f4dfa111844e321
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-08-06 14:48:20 +0700

    add scripts

--- blockend/tasks/deploy/mocks/deployCoreMock.js
+++ blockend/tasks/deploy/mocks/deployCoreMock.js
@@ -1,1 +1,70 @@
-    
+const { networks } = require("../../networks");
+
+task("deploy-core-mock", "Deploys the DropifyCoreMock contract")
+  .addOptionalParam(
+    "verify",
+    "Set to true to verify contract",
+    false,
+    types.boolean
+  )
+  .setAction(async (taskArgs) => {
+    console.log(`Deploying DropifyCoreMock contract to ${network.name}`);
+
+    console.log("\n__Compiling Contracts__");
+    await run("compile");
+
+    const dropifyCoreMockContractFactory = await ethers.getContractFactory("DropifyCoreMock");
+    const dropifyCoreMockContract = await dropifyCoreMockContractFactory.deploy();
+
+    console.log(
+      `\nWaiting ${
+        networks[network.name].confirmations
+      } blocks for transaction ${
+        dropifyCoreMockContract.deployTransaction.hash
+      } to be confirmed...`
+    );
+
+    await dropifyCoreMockContract.deployTransaction.wait(
+      networks[network.name].confirmations
+    );
+
+    console.log("\nDeployed DropifyCoreMock contract to:", dropifyCoreMockContract.address);
+
+    if (network.name === "localFunctionsTestnet") {
+      return;
+    }
+
+    const verifyContract = taskArgs.verify;
+    if (
+      network.name !== "localFunctionsTestnet" &&
+      verifyContract &&
+      !!networks[network.name].verifyApiKey &&
+      networks[network.name].verifyApiKey !== "UNSET"
+    ) {
+      try {
+        console.log("\nVerifying contract...");
+        await run("verify:verify", {
+          address: dropifyCoreMockContract.address,
+          constructorArguments: [],
+        });
+        console.log("Contract verified");
+      } catch (error) {
+        if (!error.message.includes("Already Verified")) {
+          console.log(
+            "Error verifying contract.  Ensure you are waiting for enough confirmation blocks, delete the build folder and try again."
+          );
+          console.log(error);
+        } else {
+          console.log("Contract already verified");
+        }
+      }
+    } else if (verifyContract && network.name !== "localFunctionsTestnet") {
+      console.log(
+        "\nPOLYGONSCAN_API_KEY, ETHERSCAN_API_KEY or FUJI_SNOWTRACE_API_KEY is missing. Skipping contract verification..."
+      );
+    }
+
+    console.log(
+      `\n DropifyCoreMock contract deployed to ${dropifyCoreMockContract.address} on ${network.name}`
+    );
+  });
