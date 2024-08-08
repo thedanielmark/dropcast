39566a62ae5a8bc3a6509f9dd8b6ee695f004a86
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-08-08 06:39:50 +0530

    Revert "add alchemy"
    This reverts commit 0e737c821983aae88a36b61cee5836f883857cae.

--- app/src/app/playground.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function PlaygroundPage() {
-  return (
-    <div>
-      <div className="text-2xl font-bold">Create an Airdrop</div>
-    </div>
-  );
-}
--- app/src/components/providers/MainProvider/MainProvider.tsx
+++ app/src/components/providers/MainProvider/MainProvider.tsx
@@ -1,19 +1,8 @@
 import { ReactNode } from 'react';
-import { cookieToInitialState } from '@alchemy/aa-alchemy/config';
-import { config, queryClient } from '@/utilities/alchemy-config';
-import { headers } from 'next/headers';
-import { AlchemyAccountProvider, AlchemyAccountsProviderProps } from '@alchemy/aa-alchemy/react';
 
 interface Props {
   children: ReactNode;
 }
 
 // This is the place responsible for grouping all providers from the app
-export const MainProvider = ({ children }: Props) => {
-  const initialState = cookieToInitialState(config, headers().get('cookie') ?? undefined);
-  return (
-    <AlchemyAccountProvider config={config} queryClient={queryClient} initialState={initialState}>
-      {children}
-    </AlchemyAccountProvider>
-  );
-};
+export const MainProvider = ({ children }: Props) => <div>{children}</div>;
