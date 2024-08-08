0e737c821983aae88a36b61cee5836f883857cae
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-08-08 06:35:44 +0530

    add alchemy

---
+++ app/src/app/playground.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function PlaygroundPage() {
+  return (
+    <div>
+      <div className="text-2xl font-bold">Create an Airdrop</div>
+    </div>
+  );
+}
--- app/src/components/providers/MainProvider/MainProvider.tsx
+++ app/src/components/providers/MainProvider/MainProvider.tsx
@@ -1,8 +1,19 @@
 import { ReactNode } from 'react';
+import { cookieToInitialState } from '@alchemy/aa-alchemy/config';
+import { config, queryClient } from '@/utilities/alchemy-config';
+import { headers } from 'next/headers';
+import { AlchemyAccountProvider, AlchemyAccountsProviderProps } from '@alchemy/aa-alchemy/react';
 
 interface Props {
   children: ReactNode;
 }
 
 // This is the place responsible for grouping all providers from the app
-export const MainProvider = ({ children }: Props) => <div>{children}</div>;
+export const MainProvider = ({ children }: Props) => {
+  const initialState = cookieToInitialState(config, headers().get('cookie') ?? undefined);
+  return (
+    <AlchemyAccountProvider config={config} queryClient={queryClient} initialState={initialState}>
+      {children}
+    </AlchemyAccountProvider>
+  );
+};
